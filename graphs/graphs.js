4b9a1f856e6565ce62a116d05dd8a09bab646087
Tim <timothy.steelej@gmail.com>
2017-12-24 14:35:42 -0800

    timDev: Added graph algos to the selection

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -1,9 +1,6 @@
 /*
 
 */
-
-
-
 class Graph {
   constructor() {
     this.vertices = [];
@@ -14,12 +11,60 @@ class Graph {
 
   }
 
-  addEdge(node) {
-
-  }
-
   removeEdge() {
 
   }
 
+}
+
+class Graph2 {
+  constructor(v) {
+    this.vertices = v;
+    this.edges = 0;
+    this.adj = [];
+
+    for (let i = 0; i < this.vertices; i++) {
+      this.adj[i] = [];
+      this.adj[i].push("");
+    }
+
+    this.addEdge = addEdge;
+    this.toString = toString;
+  }
+
+  addEdge(n1, n2) {
+    this.adj[n1].push(n2);
+    this.adj[n2].push(n1);
+    this.edges++;
+  }
+
+  removeEdge(n1, n2) {
+
+  }
+
+  addVertex(val) {
+    this.adj[this.adj.length] = [];
+  }
+
+
+  showGraph() {
+    let str = "";
+    for (let i = 0; i < this.vertices; i++) {
+      str += i + " -> ";
+      for (let k = 0; k < this.vertices; k++) {
+        if (this.adj[i][j] != undefined) {
+          str += this.adj[i][j] + ' ';
+        }
+      }
+      console.log(str);
+      str = "";
+    }
+  }
+}
+
+class Vertices {
+  constructor(data) {
+    this.data = data;
+    this.visited = false;
+  }
 }
