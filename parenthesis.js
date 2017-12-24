eacea8d88e10d04e97e105ab7fb31518fb35f5a2
Tim <timothy.steelej@gmail.com>
2017-12-24 14:51:45 -0800

    timDev: Added the parenthesis checker, if matching parenthesis are out of balance

--- parenthesis.js
+++ parenthesis.js
@@ -1,3 +1,17 @@
-function parenthesisCount() {
+function parenthesisCount(str) {
+  let stack = [];
+  let count = 0;
 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") {
+      stack.push("(");
+      count++;
+    } else if (str[i] === ")") {
+      if (stack.length > 0) {
+        stack.pop();
+        count--;
+      }
+    }
+  }
+  return stack.length + count;
 }
