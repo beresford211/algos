ba1439d2c779396064be392522381bfd5628a481
Tim <timothy.steelej@gmail.com>
2018-01-04 09:37:17 -0800

    timDev: Updated reverseLinkedList to have a recursive solution

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -33,4 +33,27 @@ var reverseList = function (head) {
   return previous;
 };
 
+var reverseListRecursively = function (head) {
+
+  // until we have 'fallen off' the end of the list
+  let iterateList = (current, previous, nextNode) => {
+    if (!current.next) {
+      current.next = previous;
+      return current;
+    }
+    // copy a pointer to the next element
+    // before we overwrite current.next
+    nextNode = current.next;
+
+    // reverse the 'next' pointer
+    current.next = previous;
+
+    // step forward in the list
+    previous = current;
+    current = nextNode;
+  }
+
+  return iterateList(head, previous, nextNode);
+};
+
 
