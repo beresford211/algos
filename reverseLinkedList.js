14905c9b6bc1f975d5eed935c65f4d43dd028d35
Tim <timothy.steelej@gmail.com>
2018-01-03 20:36:08 -0800

    timDev: Fixed issues with reverseLinkedList file

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,1 +1,36 @@
-reverseLL
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var reverseList = function (head) {
+  var current = head;
+  var previous = null;
+  var nextNode = null;
+
+  // until we have 'fallen off' the end of the list
+  while (current) {
+
+    // copy a pointer to the next element
+    // before we overwrite current.next
+    nextNode = current.next;
+
+    // reverse the 'next' pointer
+    current.next = previous;
+
+    // step forward in the list
+    previous = current;
+    current = nextNode;
+  }
+
+  return previous;
+};
+
+
